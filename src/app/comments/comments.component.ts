75e241d275305bf55931e2bdd8a6926705b73e51
saipavani68 <nagisettipavani@gmail.com>
2021-04-25 02:24:06 -0700

    dropdown styling and handling edge case scenarios

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -9,48 +9,62 @@ import { users } from '../users';
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
+
 export class CommentsComponent {
   comments = comments;
   users = users;
   isTagShown = false;
+  selectedUser: any;
+  commentToPost: any;
 
   commentsForm = this.formBuilder.group({
     comment: '',
     users: []
   });
+  
   constructor(
     private formBuilder: FormBuilder
     ) {}
 
-    autosuggestUsers(event: any): void {
-      const code = String.fromCharCode(event.keyCode);
+  autosuggestUsers(event: any): void {
+    const code = String.fromCharCode(event.keyCode);
+    let textAreaInput = event.target.value;
+    let tagLastIndex = textAreaInput.lastIndexOf('@')
 
-      // The below line checks that if you are starting a new word reset back the isTagShown flag
-      if(code === ' ') {
-        this.isTagShown = false;
-      }
-      
-      //Resetting the users array when we type @ by clicking back
-      this.users = users;
-      let textAreaInput = event.target.value;
+    // The below line checks that if you are starting a new word or if textAreaInput is empty reset back the isTagShown flag
+    if(code === ' ' || textAreaInput.length === 0) {
+      this.isTagShown = false;
+    }
+    
+    // Resetting the users array when we type @ by clicking back
+    this.users = users;
 
-      // The below if condition checks if the current character entered is '@' and sets the isTagShown to true
-      if(Number(code) === 2) {
-        this.isTagShown = true;
-      }
-        // The below code filters users array only with values that match the value after the '@'
-        if(this.isTagShown){
-          let taggedUserValue = textAreaInput.slice(textAreaInput.lastIndexOf('@') + 1) // We are adding +1 to ignore the '@'
-          console.log(taggedUserValue);
-          if(taggedUserValue.length > 0) {
-            const result = this.users.filter(user => {
-              return user.name.toLowerCase().startsWith(taggedUserValue.toLowerCase())
-            });
+    // The below if condition checks if the current character entered is '@' and sets the isTagShown to true
+    // We want to make sure that either the beginning character is @ or a word starts with @
+    if(Number(code) === 2 && (textAreaInput.length === 1 || textAreaInput.charAt(tagLastIndex - 1) === ' ')) {
+      this.isTagShown = true;
+    }
+    
+    // The below code filters users array only with values that match the value after the '@'
+    if(this.isTagShown){
+      let taggedUserValue = textAreaInput.slice(tagLastIndex + 1) // We are adding +1 to ignore the '@'
+      if(taggedUserValue.length > 0) {
+        const result = this.users.filter(user => {
+          return user.name.toLowerCase().startsWith(taggedUserValue.toLowerCase())
+        });
         this.users = result;
       }
     }
   }
-    onSubmit(): void {
+  
+  selectUser(){
+    this.isTagShown = false;
+    let tagStartIndex = this.commentToPost.lastIndexOf('@');
+    this.commentToPost = this.commentToPost.substr(0, tagStartIndex) + this.selectedUser;
+    alert(this.selectedUser);
+  }
+    
+  onSubmit(): void {
       this.commentsForm.reset();
     }
 }
